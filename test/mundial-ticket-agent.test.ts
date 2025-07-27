daec0fe25f6273dd36072bab8c381c4e999cf485
abel <vnponce8@gmail.com>
2025-07-27 01:25:00 -0600

    Initial commit

---
+++ test/mundial-ticket-agent.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MundialTicketAgent from '../lib/mundial-ticket-agent-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mundial-ticket-agent-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MundialTicketAgent.MundialTicketAgentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
